b2eab52ebb83151b558e93c79a169e92ede500f0
SyedaAnshrahGillani <90501474+SyedaAnshrahGillani@users.noreply.github.com>
2025-09-20 16:20:27 +0500

    Update train.py

--- software_arch_distill/train.py
+++ software_arch_distill/train.py
@@ -53,7 +53,7 @@ processed_data = dataset.map(extract_fields, remove_columns=dataset.column_names
 # 3️⃣ Tokenizer
 # -------------------------------
 student_model_name = "Qwen/Qwen3-4B-Thinking-2507"
-teacher_model_name = "Kimi-K2-0905"
+teacher_model_name = "moonshotai/Kimi-K2-Instruct-0905"
 
 tokenizer = AutoTokenizer.from_pretrained(student_model_name)
 
